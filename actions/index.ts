e114f8ab60eff29ffa873982d6a24aa9b85891ff
wasiqs-classics <was_iq87@hotmail.com>
2024-07-30 20:54:15 +0500

    RAG is now Functional

--- actions/index.ts
+++ actions/index.ts
@@ -8,10 +8,16 @@ const openAI = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
 
+// FAQ Type - Creating a new type here
+type FAQ = {
+    question: string;
+    answer: string;
+}
+
 
 // Read the FAQs JSON File
 const filePath = path.resolve(process.cwd(), 'data', 'faqs.json');
-const faqs = JSON.parse(fs.readFileSync(filePath, 'utf-8')).faqs;
+const faqs: FAQ[] = JSON.parse(fs.readFileSync(filePath, 'utf-8')).faqs;
 console.log(faqs);
 
 /**
@@ -25,8 +31,25 @@ export async function chatCompletion(chatMessages: Message[]) {
         
         console.log('FROM BACKEND: ', chatMessages);  
 
+        // Checking if the user question is in the FAQ array
+
+        const faqsAnswer = faqs.find(faq => chatMessages.at(-1)?.content.toLowerCase().includes(faq.question.toLowerCase()));
+
+        if (faqsAnswer) {
+            console.log(faqsAnswer);
+            return {role: 'assistant', content: faqsAnswer.answer} as Message;
+        }
+
+        console.log('Reaching out to OPENAI API ...');
+        
+        // Chat to be sent to OPEN AI
+
         const chat = [
             {role: 'system', content: 'You are a helpful assistant'},
+            ...faqs.map(faq => ({
+                role: 'system',
+                content: `Q: ${faq.question}\nA: ${faq.answer}`
+            })),
             ...chatMessages
         ];
     
@@ -34,11 +57,23 @@ export async function chatCompletion(chatMessages: Message[]) {
             messages: chat,
             model: 'gpt-4o-mini'
         });
+
+        if (!completion) {
+            throw new Error("Invalid response from OPENAI API!");
+        }
+
+        // Bot / Assistant Message
+        const assistantMesage = completion.choices[0].message?.content;
+
+        if (!assistantMesage) {
+            throw new Error("No message from OPENAI API");
+        }
     
         console.log('COMPLETION', completion.choices[0]);
-        return completion;
+        return {role: 'assistant', content: assistantMesage} as Message;
         
     } catch (error) {
         console.log(error);
+        return {role: 'assistant', content: "I'm sorry, something went wrong. Please try again later."} as Message;
     }
 }
